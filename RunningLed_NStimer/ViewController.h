3f803a0b2f7158af8673702f09e8dfc16a59c01b
Dang Vu Duy <duydang@duys-macbook-air.local>
2015-12-06 19:40:53 +0700

    Initial Commit

---
+++ RunningLed_NStimer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RunningLed_NStimer
+//
+//  Created by Duy Dang on 12/6/15.
+//  Copyright © 2015 Duy Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RunningLed_NStimer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  RunningLed_NStimer
+//
+//  Created by Duy Dang on 12/6/15.
+//  Copyright © 2015 Duy Dang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
